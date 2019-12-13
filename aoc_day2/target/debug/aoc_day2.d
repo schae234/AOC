0604bc59a5c9efef6e7bb6d34b83b3962948a67d
Rob <rob@linkage.io>
2019-12-13 08:42:04 -0700

    day 2 progress

--- aoc_day2/target/debug/aoc_day2.d
+++
@@ -1,1 +0,0 @@
-/home/rob/Codes/aoc2019/aoc_day2/target/debug/aoc_day2: /home/rob/Codes/aoc2019/aoc_day2/src/main.rs
--- aoc_day2/target/debug/deps/aoc_day2-0230cf5216e79355.d
+++
@@ -1,5 +0,0 @@
-/home/rob/Codes/aoc2019/aoc_day2/target/debug/deps/aoc_day2-0230cf5216e79355: src/main.rs
-
-/home/rob/Codes/aoc2019/aoc_day2/target/debug/deps/aoc_day2-0230cf5216e79355.d: src/main.rs
-
-src/main.rs:
