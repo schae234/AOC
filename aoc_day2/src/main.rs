12264b6953f1e0af790525a8cc768d25491baff9
Rob <rob@linkage.io>
2019-12-13 15:49:20 -0700

    Day2 Part 1 works

--- aoc_day2/src/main.rs
+++ aoc_day2/src/main.rs
@@ -1,37 +1,83 @@
 use std::fs;
 use std::env;
-use std::error::Error;
-use std::{error, fmt};
-use std::fmt::Display;
+use std::io::{Error, ErrorKind};
 
-
-#[derive(Debug)]
-struct ArgError {
-    v: String,
+struct Computer {
+    ptr: usize,
+    codes: Vec<i64>
 }
 
-impl ArgError {
-    fn new() -> ArgError {
-        ArgError{
-            v: "Please provide only 1 argument: a file with opCodes".to_string()
+impl Computer {
+
+    fn new(codes: Vec<i64>) -> Computer {
+        return Computer{
+            ptr: 0,
+            codes: codes
+        } 
+    }
+
+    fn len(&self) -> usize {
+        return self.codes.len()
+    }
+
+    fn current_code(&self) -> i64 {
+        self.codes[self.ptr]
+    }
+
+    fn add(&mut self) -> () {
+        let x = self.codes[self.codes[self.ptr+1] as usize];
+        let y = self.codes[self.codes[self.ptr+2] as usize];
+        let i = self.codes[self.ptr+3] as usize;
+        self.codes[i] = x + y;
+        ()
+    }
+
+    fn mult(&mut self) -> () {
+        let x = self.codes[self.codes[self.ptr+1] as usize];
+        let y = self.codes[self.codes[self.ptr+2] as usize];
+        let i = self.codes[self.ptr+3] as usize;
+        self.codes[i] = x * y;
+        ()
+       
+    }
+
+    fn run(&mut self) -> i64 {
+        loop {
+            println!("Current program state: {:?}",self.codes);
+            let instruction = self.current_code();
+            match instruction {
+                1 => {
+                    println!("Adding! Found a {} at {}", instruction, self.ptr);
+                    self.add(); 
+                },
+                2 => {
+                    println!("Multiplying! Found a {} at {}", instruction, self.ptr);
+                    self.mult();
+
+                },
+                99 => {
+                    println!("Aborting!, Found a {} at {}", instruction, self.ptr);
+                    break;
+                },
+                _ => {
+                    panic!("Bad instruction")
+                }
+            }
+
+            println!("Current pointer is: {}", self.ptr);
+            println!("Current code is: {}",self.current_code());
+            self.ptr += 4;
         }
+        return self.codes[0]
     }
+
 }
 
-impl error::Error for ArgError {
-    fn description(&self) -> &str { &self.v }
-}
 
-impl Display for ArgError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "MyError: {}", &self.v)
-    }
-}
-
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<(), Error> {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
-        return Box::new(ArgError::new())
+        return Err(Error::new(ErrorKind::InvalidInput,"Invalid number of arguments"))
     }
     else{
         let mass_file = &args[1];
@@ -39,13 +85,44 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         // we can use the ? operator here 
         let opcodes = fs::read_to_string(&mass_file)?;
-        let opcodes: Vec<u32> = opcodes
+        let opcodes: Vec<i64> = opcodes
             .trim()
             .split(',')
             .map(|x| x.parse().unwrap()).collect();
 
-        println!("Found {:?} OpCodes",opcodes.len());
+        let mut c = Computer::new(opcodes);
+
+        println!("Found {:?} OpCodes",c.len());
+        
+        let output = c.run();
+        println!("Program ended with a {} at position 0",output);
 
     }
     Ok(())
+}
+
+#[test]
+fn t1() {
+    let mut c = Computer::new(vec!(1,0,0,0,99));
+    let x = c.run();
+    assert_eq!(x,2);
+}
+
+#[test]
+fn t2() {
+   let mut c = Computer::new(vec!(2,3,0,3,99)); 
+   let x = c.run();
+   assert_eq!(x,2);
+}
+#[test]
+fn t3() {
+    let mut c = Computer::new(vec!(1,1,1,4,99,5,6,0,99));
+    let x = c.run();
+    assert_eq!(x,30);
+}
+#[test]
+fn t4() {
+    let mut c = Computer::new(vec!(1,9,10,3,2,3,11,0,99,30,40,50));
+    let x = c.run();
+    assert_eq!(x,3500);
 }
