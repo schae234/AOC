938486bef862c5740a860575fba51848c2c49f61
goeringr <raeanngoering23@gmail.com>
2022-12-26 11:22:31 -0700

    day3 solution
    fixed the problems

--- 2022/python/Day3.py
+++ 2022/python/Day3.py
@@ -12,6 +12,20 @@ import string
 from itertools import islice
 
 def split_contents(contents_string, compartment):
+    """
+
+    Parameters
+    ----------
+    contents_string : str
+    compartment : int
+
+    Returns
+    -------
+    contents : str
+
+        This function splits a load into either of its two compartments
+
+    """
     if compartment == 1:
         contents = contents_string[0:int(len(contents_string)/2)]
     elif compartment == 2:
@@ -20,6 +34,20 @@ def split_contents(contents_string, compartment):
 
 
 def find_missorted(contents1, contents2):
+    """
+
+    Parameters
+    ----------
+    contents1 : str
+    contents2 : str
+
+    Returns
+    -------
+    missorted : str
+
+        This function identifies a shared element in two strings, or the missorted item.
+
+    """
     for contents in contents1:
         if contents in contents2:
             missorted = contents
@@ -27,6 +55,19 @@ def find_missorted(contents1, contents2):
 
 
 def get_priority(missorted):
+    """
+
+    Parameters
+    ----------
+    missorted : str
+
+    Returns
+    -------
+    priority : int
+
+        This function calculates the priority of the missorted item
+
+    """
     keys = list(string.ascii_lowercase[:26] + string.ascii_uppercase[:26])
     values = list(range(1, 58))
     priorities = dict(zip(keys, values))
@@ -34,6 +75,19 @@ def get_priority(missorted):
 
 
 def find_badge(group_list):
+    """
+
+    Parameters
+    ----------
+    group_list : list of length 3 containing item strings of each elf in group
+
+    Returns
+    -------
+    badge : str
+
+        This function finds the badge shared by each elf in the group
+
+    """
     contents1 = group_list[0]
     contents2 = group_list[1]
     contents3 = group_list[2]
@@ -59,11 +113,11 @@ n = 0
 
 with open('Documents/AOC/2022/python/Day3_input.txt') as f:
     while True:
-        group = [next(f).rstrip() for x in range(n+0, n+3)]
-        n += 3
-        badge = find_badge(group)
-        priority_sum += get_priority(badge)
-        if next(f) == "":
+        try:
+            group = [next(f).rstrip() for x in range(3)]
+            badge = find_badge(group)
+            priority_sum += get_priority(badge)
+        except StopIteration:
             break
 
 print(f"The sum of the group badge priorities is {priority_sum}")
