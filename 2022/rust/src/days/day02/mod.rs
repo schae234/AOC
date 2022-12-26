29ad7e8ec6e68d239272e36dc7a7ea539b44a3e1
Rob Schaefer <rob@linkage.io>
2022-12-26 11:55:09 -0700

    added part 2

--- 2022/rust/src/days/day02/mod.rs
+++ 2022/rust/src/days/day02/mod.rs
@@ -2,6 +2,7 @@ use std::cmp::Ordering;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Error};
 
+#[derive(Clone, Copy)]
 enum Throw {
     Rock,
     Paper,
@@ -41,6 +42,23 @@ impl Throw {
             _ => Ordering::Equal,
         }
     }
+    fn beater(self) -> Throw {
+        match self {
+            Throw::Rock => Throw::Paper,
+            Throw::Paper => Throw::Scissor,
+            Throw::Scissor => Throw::Rock,
+        }
+    }
+    fn loser(self) -> Throw {
+        match self {
+            Throw::Rock => Throw::Scissor,
+            Throw::Paper => Throw::Rock,
+            Throw::Scissor => Throw::Paper,
+        }
+    }
+    fn drawer(self) -> Throw {
+        self.clone()
+    }
 }
 
 struct Hand {
@@ -62,8 +80,36 @@ impl Hand {
             Ordering::Equal => 3,
             Ordering::Less => 0,
         };
-        println!("Hand score: {:?}", round_score + self.you.value());
+        //println!("Hand score: {:?}", round_score + self.you.value());
         round_score + self.you.value()
+    }
+}
+
+enum Outcome {
+    Win,
+    Draw,
+    Lose,
+}
+impl Outcome {
+    fn new(val: &str) -> Outcome {
+        match val {
+            "X" => Outcome::Lose,
+            "Y" => Outcome::Draw,
+            "Z" => Outcome::Win,
+            _ => panic!("Invalid input for new Outcome: {:?}", val),
+        }
+    }
+
+    fn hand(self, opp: Throw) -> Hand {
+        let you: Throw = match self {
+            Outcome::Win => opp.beater(),
+            Outcome::Draw => opp.drawer(),
+            Outcome::Lose => opp.loser(),
+        };
+        Hand {
+            opponent: opp,
+            you: you,
+        }
     }
 }
 
@@ -75,16 +121,24 @@ pub fn mod_main(args: Vec<String>) -> Result<(), Error> {
         let buffered = BufReader::new(input);
 
         let mut total_score = 0;
+        let mut round2_score = 0;
         for line_res in buffered.lines() {
             let line = line_res?;
             let fields: Vec<&str> = line.trim().split(" ").collect();
             if fields.len() != 2 {
                 panic!("Incorrect number of throws per line: {:?}", fields)
             }
+            // Round 1 score
             let hand = Hand::new(fields[0], fields[1]);
             total_score += hand.score();
+
+            // Round 2 score
+            let desired_outcome = Outcome::new(fields[1]);
+            let desired_hand = desired_outcome.hand(Throw::new(fields[0]));
+            round2_score += desired_hand.score();
         }
-        println!("Total score: {:?}", total_score)
+        println!("Total round 1 score: {:?}", total_score);
+        println!("Total round 2 score: {:?}", round2_score);
     }
     Ok(())
 }
